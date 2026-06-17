1b1eeabd329dc972d4be911b77b2d523a20f457b
Shiv Pratap Singh Chandel <shivpratap1122005@gmail.com>
2026-06-18 00:28:21 +0530

    feat:Added error handling for dir_parser

--- main.cpp
+++ main.cpp
@@ -10,52 +10,53 @@ void dir_parser(std::vector<std::string> &files, std::string curr) {
   const std::vector<std::string> exDir = {"build", ".git", ".venv"};
   fs::path base_dir{curr};
 
-  auto start = fs::recursive_directory_iterator(curr);
-  auto end = fs::recursive_directory_iterator();
+  try {
+    auto start = fs::recursive_directory_iterator(curr);
+    auto end = fs::recursive_directory_iterator();
 
-  while (start != end) {
-    if (start->is_regular_file()) {
-      fs::path target_file{start->path()};
-      fs::path rel_path = fs::relative(target_file, base_dir);
-      files.push_back(rel_path.string());
-    }
-    if (start->is_directory()) {
-      std::string dirName = start->path().filename().string();
-      if (std::find(exDir.begin(), exDir.end(), dirName) != exDir.end()) {
-        start.disable_recursion_pending();
-        start++;
-        continue;
-      } 
-      else {
+    while (start != end) {
+      if (start->is_regular_file()) {
         fs::path target_file{start->path()};
         fs::path rel_path = fs::relative(target_file, base_dir);
         files.push_back(rel_path.string());
       }
+      if (start->is_directory()) {
+        std::string dirName = start->path().filename().string();
+        if (std::find(exDir.begin(), exDir.end(), dirName) != exDir.end()) {
+          start.disable_recursion_pending();
+        } else {
+          fs::path target_file{start->path()};
+          fs::path rel_path = fs::relative(target_file, base_dir);
+          files.push_back(rel_path.string());
+        }
+      }
+      start++;
     }
-    start++;
+  } catch (const fs::filesystem_error &e) {
+    std::cerr << "Error: " << e.what() << "\n";
+  } catch (const std::exception &e) {
+    std::cerr << "Error: " << e.what() << "\n";
   }
 }
 
 int fuzzy_score(const std::string &file_name, const std::string &query) {
   int score = 0;
-  int last_match_pos = -1;
+  int last_match_pos = -2;
   int query_pointer = 0;
   int file_name_pointer = 0;
-  int level_counter = 0;
+  int level_counter;
   if (query.length() == 0) return 0;
   while (file_name_pointer != (file_name.length())) {
     char small_query = std::tolower(query[query_pointer]);
     char small_file_name = std::tolower(file_name[file_name_pointer]);
     if (small_query == small_file_name) {
-      if (file_name_pointer - last_match_pos == 1) {
-        score += 1;
-      }
-      if (query[query_pointer] == file_name[file_name_pointer]) {
-        score += 1;
-      } 
-      else {
+      score++;
+      if ((file_name_pointer - last_match_pos) == 1) {
         score++;
       }
+      if (query[query_pointer] == file_name[file_name_pointer]) {
+        score++;
+      } 
       last_match_pos = file_name_pointer;
       query_pointer++;
       if (query_pointer == query.length()) {
@@ -73,11 +74,14 @@ int fuzzy_score(const std::string &file_name, const std::string &query) {
 }
 
 int main() {
-  std::string curr;
-  std::getline(std::cin, curr);
+  std::string curr="/Users/shivpratapsinghchandel/Random";
+  //std::getline(std::cin, curr);
   std::vector<std::string> file_names;
   dir_parser(file_names, curr);
-  std::string query;
-  std::getline(std::cin, query);
+  std::string query="line";
+  //std::getline(std::cin, query);
+  for (std::string file_name:file_names){
+      std::cout<<file_name<<": "<<fuzzy_score(file_name, query)<<std::endl;
+  }
   return 0;
 }
